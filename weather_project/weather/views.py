a1f6f0fd033aa88ac43242e2812d4f8c71affb6c
NadinKon <nadinkahf@gmail.com>
2024-07-17 11:38:47 +0300

    added error handling

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -22,23 +22,32 @@ class IndexView(View):
                 response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
                 response.raise_for_status()
                 res = response.json()
-                weather_data = {
-                    'city': city,
-                    'temperature': round(res['main']['temp']),
-                    'description': res['weather'][0]['description'],
-                    'icon': res['weather'][0]['icon']
-                }
-                # Update SearchHistory
-                search_history, created = SearchHistory.objects.get_or_create(city=city)
-                search_history.search_count += 1
-                search_history.save()
 
-                # Save UserSearchHistory if user is authenticated
-                if request.user.is_authenticated:
-                    UserSearchHistory.objects.create(user=request.user, city=city)
+                if 'main' in res and 'temp' in res['main'] and 'weather' in res and len(res['weather']) > 0:
+                    weather_data = {
+                        'city': city,
+                        'temperature': round(res['main']['temp']),
+                        'description': res['weather'][0]['description'],
+                        'icon': res['weather'][0]['icon']
+                    }
+                    # Update SearchHistory
+                    search_history, created = SearchHistory.objects.get_or_create(city=city)
+                    search_history.search_count += 1
+                    search_history.save()
 
+                    # Save UserSearchHistory if user is authenticated
+                    if request.user.is_authenticated:
+                        UserSearchHistory.objects.create(user=request.user, city=city)
+                else:
+                    error = f"No weather data found for {city}. Please enter a valid city name."
+
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 404:
+                    error = f"City '{city}' not found. Please enter a valid city name."
+                else:
+                    error = f"An error occurred while retrieving weather data: {str(e)}"
             except requests.exceptions.RequestException as e:
-                error = str(e)
+                error = f"An error occurred while retrieving weather data: {str(e)}"
 
         context = {
             'weather_data': weather_data,
